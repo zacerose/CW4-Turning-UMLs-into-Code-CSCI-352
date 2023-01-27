8cebe592ab511b5c467ce4d9d4a6b26e0a1d9ab7
Zachary Rose <zacerose@ut.utm.edu>
2023-01-27 11:41:49 -0600

    Added abstract project to see how it looks on Github.

---
+++ CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW4_Turning_UMLs_into_Code_Abstract_Pattern_CSCI352
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
