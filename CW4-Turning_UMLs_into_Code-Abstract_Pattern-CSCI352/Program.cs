9c1423ae98e2d38a5f4fbff0901a66c9f6fe3e37
Zachary Rose <zacerose@ut.utm.edu>
2023-01-27 11:48:51 -0600

    Added some boiler plate for abstract factory.

--- CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/Program.cs
+++ CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/Program.cs
@@ -8,6 +8,28 @@ namespace CW4_Turning_UMLs_into_Code_Abstract_Pattern_CSCI352
 {
     internal class Program
     {
+        enum Manufacturers
+        {
+            SAMSUNG, 
+            HTC,
+            NOKIA
+        }
+        interface IDumb
+        {
+            string getName();
+        }
+        
+        interface ISmart
+        {
+            string getName();
+        }
+        interface IPhoneFactory
+        {
+            ISmart GetSmart();
+            IDumb GetDumb();
+        }
+        
+        // Implement IPhoneFactory in different file?
         static void Main(string[] args)
         {
         }
