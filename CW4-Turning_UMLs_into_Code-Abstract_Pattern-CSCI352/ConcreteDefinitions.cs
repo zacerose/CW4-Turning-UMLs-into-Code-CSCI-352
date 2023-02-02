2c502a286d8ccc4f5174da7f58303e09dcfc9ae5
Zachary Rose <zacerose@ut.utm.edu>
2023-02-02 11:51:48 -0600

    Added concrete class definitions for abstract phone factory, including the phone models, phone factory, and phone checker. Filled out main, which makes all available phone checkers and checks their products. Added documentation.

---
+++ CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/ConcreteDefinitions.cs
@@ -0,0 +1,133 @@
+﻿// Name: Zachary Rose
+// Date: 2/2/2023
+// Class: CSCI352
+// Contains all concrete classes based off of the interfaces in the namespace. Each manufacterer has an associated factory, and an IDumb and ISmart phone model.
+using CW4_Turning_UMLs_into_Code_Abstract_Pattern_CSCI352;
+using System;
+
+// constructs the appropriate factory for the given manufacterer and prints out the associated phone models to console
+class PhoneTypeChecker
+{
+    public IPhoneFactory factory;
+    public Manufacturers manu;
+
+    // parameterized constructor, gives the manufacterer
+    // @Manufacterers m manufacterer to associate internal factory with
+    public PhoneTypeChecker(Manufacturers m)
+    {
+        manu = m;
+    }
+    // constructs the appropriate factory for the given manufacterer and prints out the associated phone models to console
+    public void CheckProducts()
+    {
+        switch (manu)
+        {
+            case Manufacturers.SAMSUNG:
+                factory = new SamsungFactory();
+                break;
+            case Manufacturers.HTC:
+                factory = new HTCFactory();
+                break;
+            case Manufacturers.NOKIA:
+                factory = new NokiaFactory();
+                break;
+        }
+        if (factory != null)
+        {
+            Console.WriteLine("Smart: " + factory.GetSmart().getName());
+            Console.WriteLine("Dumb: " + factory.GetDumb().getName());
+        }
+    }
+}
+// Nokia smart phone
+class Lumia : ISmart
+{
+    public string getName()
+    {
+        return "Lumia";
+    }
+}
+
+// Samsung smart phone
+class GalaxyS2 : ISmart
+{
+    public string getName()
+    {
+        return "GalaxyS2";
+    }
+}
+
+// HTC smart phone
+class Titan : ISmart
+{
+    public string getName()
+    {
+        return "Titan";
+    }
+}
+
+// Nokia dumb phone
+class Asha : IDumb
+{
+    public string getName()
+    {
+        return "Asha";
+    }
+}
+
+// HTC dumb phone
+class Genie : IDumb
+{
+    public string getName()
+    {
+        return "Genie";
+    }
+}
+
+// Samsung dumb phone
+class Primo : IDumb
+{
+    public string getName()
+    {
+        return "Primo";
+    }
+}
+
+// Samsung phone factory, can return the related smart and dumb phone models
+class SamsungFactory : IPhoneFactory
+{
+    public ISmart GetSmart()
+    {
+        return new GalaxyS2();
+    }
+    public IDumb GetDumb()
+    {
+        return new Primo();
+    }
+}
+
+// HTC phone factory, can return the related smart and dumb phone models
+class HTCFactory : IPhoneFactory
+{
+    public ISmart GetSmart()
+    {
+        return new Titan();
+    }
+    public IDumb GetDumb()
+    {
+        return new Genie();
+    }
+}
+
+// Nokia phone factory, canr eturn the related smart and dumb phone models
+class NokiaFactory : IPhoneFactory
+{
+    public ISmart GetSmart()
+    {
+        return new Lumia();
+    }
+    public IDumb GetDumb()
+    {
+        return new Asha();
+    }
+}
--- CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/Program.cs
+++ CW4-Turning_UMLs_into_Code-Abstract_Pattern-CSCI352/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Name: Zachary Rose
+// Date: 2/2/2023
+// Class: CSCI352
+// Simple console program based off of the abstract shape factory UML. Concrete class definitions are in ConcreteDefinitions.cs
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,32 +10,48 @@ using System.Threading.Tasks;
 
 namespace CW4_Turning_UMLs_into_Code_Abstract_Pattern_CSCI352
 {
-    internal class Program
+    // all possible implemented manufacturer types, to be used in a concrete phone factory
+    enum Manufacturers
     {
-        enum Manufacturers
-        {
-            SAMSUNG, 
-            HTC,
-            NOKIA
-        }
-        interface IDumb
-        {
-            string getName();
-        }
-        
-        interface ISmart
-        {
-            string getName();
-        }
-        interface IPhoneFactory
-        {
-            ISmart GetSmart();
-            IDumb GetDumb();
-        }
-        
-        // Implement IPhoneFactory in different file?
+        SAMSUNG,
+        HTC,
+        NOKIA
+    }
+    interface IDumb
+    {
+        string getName();
+    }
+
+    interface ISmart
+    {
+        string getName();
+    }
+    interface IPhoneFactory
+    {
+        ISmart GetSmart();
+        IDumb GetDumb();
+    }
+    internal partial class Program
+    {   
+        // constructs every available factory and prints their products to console, with some extra labels and whitespace for readability
         static void Main(string[] args)
         {
+            PhoneTypeChecker phone = new PhoneTypeChecker(Manufacturers.SAMSUNG);
+            Console.WriteLine("Samsung: ");
+            phone.CheckProducts();
+            Console.WriteLine();
+
+            phone = new PhoneTypeChecker(Manufacturers.HTC);
+            Console.WriteLine("HTC: ");
+            phone.CheckProducts();
+            Console.WriteLine();
+
+            phone = new PhoneTypeChecker(Manufacturers.NOKIA);
+            Console.WriteLine("Nokia: ");
+            phone.CheckProducts();
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
